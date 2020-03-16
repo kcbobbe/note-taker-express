cf7bd915689b12e9c38d3f3705e1255480f1d7fe
kcbobbe <kcbobbe@gmail.com>
2020-03-16 17:08:25 -0400

    got basic application up and running. Just need to use fs to read and write to .db file instead of storing in a variable

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const PORT = 3000;
 
 const app = express();
 
-app.us(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // link to json db -- right now called notes
-
+const notes = require("./db/db.json")
 //html routes
 
 app.get("/", (req, res)=>{
@@ -25,9 +26,41 @@ app.get("/api/notes", (req, res)=>{
   return res.json(notes)
 })
 
+
+
+app.get("/api/notes/:id", (req, res) => {
+  // console.log(req.params.id)
+  const id = req.params.id;
+  let found;
+  notes.forEach(n => {
+    if (id == n.id){
+      found = n;
+      return res.json(n)
+    }
+  })
+  return res.json(false)
+})
+
+let uniqueID = 1;
+
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;
-  newNote.routeName = res.body.id;
+  uniqueID++;
+  newNote.id = uniqueID;
+  notes.push(newNote);
+  res.json(true)
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  let found;
+  notes.forEach((n, index) => {
+    if(id == n.id){
+      notes.splice(index,1)
+    }
+  })
+
+  res.json(true);
 })
 
 app.listen(PORT, () => {
