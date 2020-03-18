2757be20b8b8f6ac8bfe95d6894deb81308a51f9
kcbobbe <kcbobbe@gmail.com>
2020-03-18 14:06:27 -0400

    separated the css and js from the index.html file and notes.html files

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.json());
+app.use(express.static('public'));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
