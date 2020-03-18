f3d47eb20dbce3a3b6220f5cf2fa9b6a76189e83
kcbobbe <kcbobbe@gmail.com>
2020-03-18 10:52:57 -0400

    modified html route: GET * returns index.html file instead of using GET /

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,14 @@ var path = require("path");
 
 module.exports = function(app){
 
-  app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-  })
-  
   app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
   })
+
+  app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+  })
+  
+
   
 }
